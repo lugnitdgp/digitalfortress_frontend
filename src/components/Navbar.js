ff8943512a1a9fead09d7b6b7dacf5a80ce372af
Romit Karmakar <romitkarmakar@gmail.com>
2019-10-17 23:57:07 +0530

    Changed navbar logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import GoogleSignIn from '../components/GoogleSignIn';
-import logo from '../images/logo.png';
+import logo from '../images/logodf.png';
 import { Link } from "gatsby"
 import FacebookSignIn from './FacebookSignIn';
 import { navigate } from "gatsby";
