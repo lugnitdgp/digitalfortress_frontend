f84b9188ffa69ff87d022b145329b4803e16a333
Romit Karmakar <romitkarmakar@gmail.com>
2020-01-22 00:59:48 +0530

    Changed clue design

--- src/components/Clue.js
+++ src/components/Clue.js
@@ -1,45 +1,94 @@
 import React, { useState } from "react"
 import { func } from "prop-types"
 import "../custom.css"
-import { Card, CardContent, CardActionArea } from "@material-ui/core"
+import clsx from "clsx"
+import {
+  Card,
+  CardContent,
+  CardActionArea,
+  makeStyles,
+  Typography,
+  CardActions,
+  IconButton,
+  Collapse,
+  Grid,
+} from "@material-ui/core"
+import { ExpandMore } from "@material-ui/icons"
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  flex: {
+    flexGrow: 1
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}))
 
 export default props => {
-  const [collapse, setCollapse] = useState(false)
+  const classes = useStyles()
+  const [expanded, setExpanded] = React.useState(false)
   const [answer, setAnswer] = useState("")
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
   if (props.isSolved == 1) {
     var x = (
-      <CardBody>
-        <span className="ml-2">
-          Position : {props.position[1] + ", " + props.position[0]}
-        </span>
-      </CardBody>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <span className="ml-2">
+            Position : {props.position[1] + ", " + props.position[0]}
+          </span>
+        </CardContent>
+      </Collapse>
     )
   } else {
     var x = (
-      <CardBody>
-        <div className="row">
-          <div className="col col-10">
-            <input
-              className="form-control"
-              placeholder="Enter answer"
-              value={answer}
-              onChange={e => {
-                setAnswer("")
-                setAnswer(e.target.value)
-              }}
-            ></input>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <div className="row">
+            <div className="col col-10">
+              <input
+                className="form-control"
+                placeholder="Enter answer"
+                value={answer}
+                onChange={e => {
+                  setAnswer("")
+                  setAnswer(e.target.value)
+                }}
+              ></input>
+            </div>
+            <div className="col col-2">
+              <button
+                className="btn clue-submit"
+                onClick={e => props.submitClue(answer, props.id)}
+                style={{ float: "right" }}
+              >
+                Submit
+              </button>
+            </div>
           </div>
-          <div className="col col-2">
-            <button
-              className="btn clue-submit"
-              onClick={e => props.submitClue(answer, props.id)}
-              style={{ float: "right" }}
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-      </CardBody>
+        </CardContent>
+      </Collapse>
     )
   }
 
@@ -47,20 +96,25 @@ export default props => {
     <div className="mt-1">
       <Card>
         <CardContent>
-          <button
-            onClick={e => setCollapse(!collapse)}
-            className="clue-btn collapsed"
-            aria-expanded="true"
-            style={{
-              fontFamily: "'Josefin Sans', sans-serif",
-              fontSize: "1.2rem",
-            }}
-          >
-            {props.question}
-          </button>
+          <Grid container spacing={2}>
+            <Grid item xs={11}>
+            <Typography className={classes.flex}>{props.question}</Typography>
+            </Grid>
+            <Grid item xs={1}>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMore />
+            </IconButton>
+            </Grid>
+          </Grid>
           <CardActionArea>{x}</CardActionArea>
         </CardContent>
-        <Collapse isOpen={collapse}></Collapse>
       </Card>
     </div>
   )
