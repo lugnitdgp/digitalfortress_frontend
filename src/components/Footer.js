03953b153420ba54b0d62a18d4bfde4b3e441f10
Anushka7479 <50817175+Anushka7479@users.noreply.github.com>
2020-01-24 23:08:31 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import logo from '../images/glug.png';
 import nit from '../images/nit.svg';
 
 export default () => {
-    return <div className="bg-transparent p-3">
+    return <div className="bg-transparent-info">
         <span className="text-center mx-auto d-block text-white"><b>Designed and Developed by</b> GNU/Linux Users Group, NIT Durgapur</span>
         <div className="row justify-content-center">
             <img src={logo} alt="GLUG" height="50" className="m-2" />
